e710d5def49cae28cf7155a5bc1b3f49465a53ef
metalhead <pittore086+git@yandex.ru>
2024-02-19 18:25:36 +0500

    сделали сложение, умножение и деление

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,13 +1,13 @@
 ﻿int i1 = 5;
 int i2 = 6;
+long l1 = 6;
+double d1 = 6.2;
 int sum = i1 + i2;
 Console.WriteLine(sum);
 
-long l1 = 6;
 long sum2 = l1 + i1;
 Console.WriteLine(sum2);
 
-double d1 = 5.5;
 double sum3 = d1 + i1;
 Console.WriteLine(sum3);
 
@@ -18,4 +18,8 @@ Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
 
+int kurt = i1 / i2;
+Console.WriteLine(kurt);
 
+double kurt2 = i1 / d1;
+Console.WriteLine(kurt2);
