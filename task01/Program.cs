dd1332491541ae1edf77b0b390df30ab91682c6f
metalhead <pittore086+git@yandex.ru>
2024-02-19 17:53:21 +0500

    Что-то добавили

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Punk's not dead");
+﻿// Console.WriteLine("Punk's not dead");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
--- task01/obj/Debug/net8.0/task01.AssemblyInfo.cs
+++ task01/obj/Debug/net8.0/task01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0890fafa7654ab86c97b086b278062dd2af66e")]
 [assembly: System.Reflection.AssemblyProductAttribute("task01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
