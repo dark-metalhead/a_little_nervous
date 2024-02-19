b0a0cf330b4eda42acab20777c60b0d4f603f09c
metalhead <pittore086+git@yandex.ru>
2024-02-20 02:14:42 +0500

    all

--- task06/Program.cs
+++ task06/Program.cs
@@ -1,11 +1,16 @@
-﻿int first = 25;
-int second = 5;
+﻿//int first = 25;
+//int second = 5;
 
-if (second*second == first)
-{
-    Console.WriteLine("Yes");
-}
-else
-{
-    Console.WriteLine("No");
-}
+//if (second*second == first)
+//{
+//    Console.WriteLine("Yes");
+//}
+//else
+//{
+//    Console.WriteLine("No");
+//}
+
+int num = 529;
+int first = num / 100;
+int last = num % 10;
+Console.Write("Sun first and last digit is " + (first + last));
