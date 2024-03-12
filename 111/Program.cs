b179ed75b154b64c248777942e0d6a9438d50247
metalhead <pittore086+git@yandex.ru>
2024-03-12 23:01:51 +0500

    Задача №1

---
+++ 111/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая 
+//выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+static void recurs_natural(int m, int n)
+{
+    if (m > 0 && m <= n)
+    {
+        Console.WriteLine(m);
+    }
+    else if (m > 0 && m >= n)
+    {
+        Console.WriteLine(m);
+    }
+    if (m < n)
+    {
+        recurs_natural(m + 1, n);
+    }
+    else if (m > n)
+    {
+        recurs_natural(m - 1, n);
+    }
+}
+recurs_natural(20, -20);
