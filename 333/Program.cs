48937efaf0ed36310440f23c364900bdeff977c6
metalhead <pittore086+git@yandex.ru>
2024-03-12 23:13:15 +0500

    Третье задание

---
+++ 333/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 3: Задайте произвольный массив. Выведете его элементы, 
+//начиная с конца. Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class MainClass {
+public static void Main (string[] args) {
+int[] arr = {1, 2, 3, 4, 5};
+    PrintArrayFromEnd(arr, arr.Length-1);
+}
+
+static void PrintArrayFromEnd(int[] arr, int index) {
+    if (index < 0) {
+        return;
+    } else {
+        Console.WriteLine(arr[index]);
+        PrintArrayFromEnd(arr, index - 1);
+    }
+}
+}
